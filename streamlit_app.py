ee8db0448080a46ad4c02ee6d9bc0dc296367c41
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-08 01:40:59 +0530

    Update streamlit_app.py
    Fix Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My Parent's New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.next("Hard-boiled free-range egg")
+streamlit.text("Hard-boiled free-range egg")
 

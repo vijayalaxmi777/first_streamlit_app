01dd488db0d460327379dbe740ec7cec7dd79d5d
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-08 01:40:24 +0530

    Update streamlit_app.py
    Add Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("My parent's new healthy diner")
+streamlit.title("My Parent's New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.next("Hard-boiled free-range egg")
+

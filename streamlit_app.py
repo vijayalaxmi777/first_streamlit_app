a77255d5c67b1ea30b3a67397d95dc3cb789e5a2
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-09 23:40:19 +0530

    Update streamlit_app.py
    Import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,8 @@ streamlit.text("🐔 Hard-boiled free-range egg")
 streamlit.text("🥑🍞 Avacado toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruits_list)

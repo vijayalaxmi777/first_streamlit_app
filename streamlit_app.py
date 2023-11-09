66d9ae1755fd6cffa8bd158fa8b6c6da13736200
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-09 23:36:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-boiled free-range egg")
 
+streamlit.header("Breakfast Favorites")
+
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-boiled free-range egg")
+streamlit.text("🥑🍞 Avacado toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

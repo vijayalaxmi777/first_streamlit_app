c8f690c6322b045ca9863612e2c5693f9b59acc1
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-10 00:37:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.c
 #streamlit.dataframe(my_fruits_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
+#streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
 my_fruits_list = my_fruits_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include

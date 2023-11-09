9754372ed678b75163228812803e190c6a1649dd
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2023-11-09 23:49:24 +0530

    Update streamlit_app.py
    Adding a streamlit multi select picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# Reference doc - https://docs.streamlit.io/library/api-reference
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
@@ -15,3 +16,6 @@ import pandas
 
 my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruits_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multipicker(f"Pick some fruits: {list(my_fruits_list.index)}")

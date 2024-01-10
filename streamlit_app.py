0a08894812587bbd2a2131034718a7c748f18216
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2024-01-11 02:24:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ fruits_to_show = my_fruits_list.loc[fruits_selected]  # for loc fun in pandas - 
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice..")
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
